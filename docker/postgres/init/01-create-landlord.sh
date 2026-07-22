9eca726bff79e78822bb090eac224ab8ab793319
xmdn <tnebilyk@gmail.com>
2026-07-22 21:20:29 +0000

    Add database creation script for landlord database and update pg_hba.conf

--- docker/postgres/init/01-create-landlord.sh
+++ docker/postgres/init/01-create-landlord.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -eu
 
+LANDLORD_DB_DATABASE="${LANDLORD_DB_DATABASE:-noma_landlord}"
+
+sed -i 's/^host all all all scram-sha-256$/host all all all trust/' /var/lib/postgresql/data/pg_hba.conf
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<SQL
 CREATE DATABASE "$LANDLORD_DB_DATABASE";
 SQL
